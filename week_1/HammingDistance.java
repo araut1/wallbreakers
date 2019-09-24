f4c503cd2a303ee44cdb38c8bf768f6cb939500c
Abhishek Dinkar Raut <9390583+araut1@users.noreply.github.com>
2019-09-24 01:35:19 -0400

    Leetcode 461. Hamming Distance

---
+++ week_1/HammingDistance.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int z = x ^ y, ans = 0;
+        while (z != 0) {
+            if ((z & 1) == 1) {
+                ans++;
+            }
+            z >>= 1;
+        }
+        return ans;
+}
+}
