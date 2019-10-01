18aad25214eec97c1813020bd001623c1c87b9bf
Abhishek Dinkar Raut <9390583+araut1@users.noreply.github.com>
2019-10-01 00:04:03 -0400

    389. Find the Difference

---
+++ week_2/find-the-difference.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    public char findTheDifference(String s, String t) {
+
+        Multiset<Character> sMultiset = new Multiset<>();
+        for (int i = 0; i < s.length(); i++) {
+            sMultiset.add(s.charAt(i));
+        }
+        Multiset<Character> tMultiset = new Multiset<>();
+        for (int i = 0; i < t.length(); i++) {
+            tMultiset.add(t.charAt(i));
+        }
+
+        if (sMultiset.elementSet().size() > tMultiset.elementSet().size()) {
+            
+            return getExtraLetter(sMultiset, tMultiset);
+            
+        } else if (tMultiset.elementSet().size() > sMultiset.elementSet().size()) {
+            
+            return getExtraLetter(tMultiset, sMultiset);
+            
+        }
+
+
+        for (Character c : sMultiset.elementSet()) {
+            if (sMultiset.count(c) != tMultiset.count(c)) {
+                return c;
+            }
+        }
+        return ' ';
+    }
+
+    public char getExtraLetter(Multiset<Character> larger, Multiset<Character> smaller) {
+        for (Character c : larger.elementSet()) {
+            if (!smaller.contains(c)) {
+                return c;
+            }
+        }
+        return ' ';
+    }
+}
